cf424801ffdb69fa1d0775c0254d492df732b16e
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-11 19:54:37 +0300

    setup route et creation components

---
+++ front/src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from "../Header/Header"
+import Routers from "../../routers/Routers"
+
+const Layout = () => {
+  return (
+    <>
+        <Header/>
+        <div>
+            <Routers/>
+        </div>
+    </>
+  )
+}
+
+export default Layout
